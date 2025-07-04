d80cb232f7913a805bbee66abac3d3a06785a86d
SOURAV SINGHAL <souravsinghal2004@gmail.com>
2025-07-04 12:44:26 +0530

    fixing image rendering

--- app/page.jsx
+++ app/page.jsx
@@ -90,14 +90,14 @@ export default async function Home() {
                 className="bg-white rounded-lg shadow p-4 text-center hover:shadow-md transition cursor-pointer"
               >
                 <div className="h-16 w-auto mx-auto mb-2 relative">
-                  <Image
-                    src={
-                      make.imageUrl || `/make/${make.name.toLowerCase()}.webp`
-                    }
-                    alt={make.name}
-                    fill
-                    style={{ objectFit: "contain" }}
-                  />
+                 <Image
+  src={make.imageUrl || `/make/${make.name.toLowerCase()}.webp`}
+  alt={make.name}
+  fill
+  style={{ objectFit: "contain" }}
+  unoptimized // 👈 ADD THIS
+/>
+
                 </div>
                 <h3 className="font-medium">{make.name}</h3>
               </Link>
